2d815e753adc4e5c42798bf8842afef12f51b9fc
minhhieu181020 <nguyenminhhieu181020@gmail.com>
2025-09-20 12:30:58 +0700

    fix: change host

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // ⚡ chỉnh port cho khớp backend
+  baseURL: "https://flashcard-backend-0dwg.onrender.com", // ⚡ chỉnh port cho khớp backend
 });
 
 export default api;
