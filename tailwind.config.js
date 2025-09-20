08453c731c32d6d72877283de72eb916e9156f8b
minhhieu181020 <nguyenminhhieu181020@gmail.com>
2025-09-20 12:19:26 +0700

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"]
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // ⚡ quét toàn bộ file trong src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
